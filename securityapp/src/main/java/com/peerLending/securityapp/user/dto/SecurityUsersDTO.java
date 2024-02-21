d300e2d045e48482d6d2caf7f0056b2c302415c9
Troy.Andati <andatisiro@gmail.com>
2024-02-21 17:15:45 +0300

    userDto

--- securityapp/src/main/java/com/peerLending/securityapp/user/dto/SecurityUsersDTO.java
+++ securityapp/src/main/java/com/peerLending/securityapp/user/dto/SecurityUsersDTO.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SecurityUsersDTO {
-
   private String username;
   private String password;
   private String firstName;
